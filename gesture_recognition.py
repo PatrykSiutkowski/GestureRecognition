47722cfd39814c511d422774933cf36a20d20f99
Patryk Siutkowski <psiutkowski@gmail.com>
2026-03-05 19:09:22 +0000

    code now works on images, but image is not produced

--- gesture_recognition.py
+++ gesture_recognition.py
@@ -9,9 +9,9 @@ GestureRecognizer = mp.tasks.vision.GestureRecognizer
 GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 
-model_path = 'home/patryksiutkowski/GitHub/GestureRecognition/gesture_recognizer.task'
-base_options = BaseOptions(model_asset_path=model_path)
-mp_image = "home/patryksiutkowski/GitHub/GestureRecognition/Photo_from_2026-03-05_18-28-41.977291.jpeg"
+model_path = "/home/patryksiutkowski/GitHub/GestureRecognition/gesture_recognizer.task"
+mp_image = mp.Image.create_from_file("/home/patryksiutkowski/GitHub/GestureRecognition/Photo_from_2026-03-05_18-28-41.977291.jpeg")
+#mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=numpy_image)
 
 BaseOptions = mp.tasks.BaseOptions
 GestureRecognizer = mp.tasks.vision.GestureRecognizer
@@ -25,3 +25,4 @@ options = GestureRecognizerOptions(
 
 with GestureRecognizer.create_from_options(options) as recognizer:
   gesture_recognition_result = recognizer.recognize(mp_image)
+  print(gesture_recognition_result)

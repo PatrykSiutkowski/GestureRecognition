c053abbf445bc529a5418ee014070d63ff17b0ef
Patryk Siutkowski <psiutkowski@gmail.com>
2026-03-05 18:41:01 +0000

    init implement of recog from image

--- gesture_recognition.py
+++ gesture_recognition.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
-
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+
+BaseOptions = mp.tasks.BaseOptions
+GestureRecognizer = mp.tasks.vision.GestureRecognizer
+GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+model_path = 'home/patryksiutkowski/GitHub/GestureRecognition/gesture_recognizer.task'
+base_options = BaseOptions(model_asset_path=model_path)
+mp_image = "home/patryksiutkowski/GitHub/GestureRecognition/Photo_from_2026-03-05_18-28-41.977291.jpeg"
+
+BaseOptions = mp.tasks.BaseOptions
+GestureRecognizer = mp.tasks.vision.GestureRecognizer
+GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+# Create a gesture recognizer instance with the image mode:
+options = GestureRecognizerOptions(
+  base_options=BaseOptions(model_asset_path=model_path),
+  running_mode=VisionRunningMode.IMAGE)
+
+with GestureRecognizer.create_from_options(options) as recognizer:
+  gesture_recognition_result = recognizer.recognize(mp_image)

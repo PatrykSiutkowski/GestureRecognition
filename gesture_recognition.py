3c9b76d7072804f5d25f816c75d7891f2b2d421b
Patryk Siutkowski <psiutkowski@gmail.com>
2026-03-21 23:41:33 +0000

    cleaned up code

--- gesture_recognition.py
+++ gesture_recognition.py
@@ -1,79 +1,75 @@
 #!/usr/bin/env python3
 
 import mediapipe as mp
-import os
 import cv2
 import time
 
-BaseOptions = mp.tasks.BaseOptions
-GestureRecognizer = mp.tasks.vision.GestureRecognizer
+BaseOptions              = mp.tasks.BaseOptions
+GestureRecognizer        = mp.tasks.vision.GestureRecognizer
 GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
-VisionRunningMode = mp.tasks.vision.RunningMode
+VisionRunningMode        = mp.tasks.vision.RunningMode
 
 model_path = "gesture_recognizer.task"
 image_path = "photo_from_2026_03_21_22_51_51.094174.jpeg"
 
-if not os.path.exists(model_path):
-  raise FileNotFoundError("Model file not found")
+def print_result(result, output_image, timestamp_ms):
+  if result.gestures:
+    for gesture_list in result.gestures:
+      for gesture in gesture_list:
+        print(f"{gesture.category_name}: {gesture.score:.2f}")
+  else:
+    print("No gestures detected")
+
+def image_mode():
+  mp_image = mp.Image.create_from_file(image_path)
+    
+  options = GestureRecognizerOptions(
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.IMAGE)
+
+  with GestureRecognizer.create_from_options(options) as recognizer:
+    result = recognizer.recognize(mp_image)
+    print_result(result, output_image=..., timestamp_ms=...)
+
+def livestream_mode():
+  options = GestureRecognizerOptions(
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+
+  cap = cv2.VideoCapture(0)
+
+  with GestureRecognizer.create_from_options(options) as recognizer:
+    while cap.isOpened():
+      ret, frame = cap.read()
+      if not ret:
+        break
+  
+      rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Convert BGR (OpenCV) to RGB (MediaPipe)
+
+      mp_image = mp.Image(
+        image_format=mp.ImageFormat.SRGB,
+        data=rgb_frame)
+
+      timestamp_ms = int(time.time() * 1000) # Timestamp required for live mode
+      recognizer.recognize_async(mp_image, timestamp_ms)
+      
+      cv2.imshow("Gesture Recognition", frame) # Show camera feed
+
+      if cv2.waitKey(1) & 0xFF == 27:  # exit using ESC key
+        break
+
+  cap.release()
+  cv2.destroyAllWindows()
 
 if __name__ == "__main__":
   vision_running_mode_selection = input("IMAGE or LIVESTREAM?: ")
 
   if vision_running_mode_selection.lower() == "image":
-    
-    mp_image = mp.Image.create_from_file(image_path)
-    
-    options = GestureRecognizerOptions(
-    base_options=BaseOptions(model_asset_path=model_path),
-    running_mode=VisionRunningMode.IMAGE)
-
-    with GestureRecognizer.create_from_options(options) as recognizer:
-      result = recognizer.recognize(mp_image)
-
-    if result.gestures:
-      for gesture_list in result.gestures:
-        for gesture in gesture_list:
-          print(f"{gesture.category_name}: {gesture.score}")
-    else:
-        print("No gestures detected")
+    image_mode()
   
   elif vision_running_mode_selection.lower() == "livestream":
-    def print_result(result, output_image, timestamp_ms):
-      if result.gestures:
-        for gesture_list in result.gestures:
-          for gesture in gesture_list:
-            print(f"{gesture.category_name}: {gesture.score:.2f}")
+    livestream_mode()
 
-    options = GestureRecognizerOptions(
-    base_options=BaseOptions(model_asset_path=model_path),
-    running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result)
-
-    cap = cv2.VideoCapture(0)
-
-    with GestureRecognizer.create_from_options(options) as recognizer:
-      while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-          break
-
-        # Convert BGR (OpenCV) → RGB (MediaPipe)
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        mp_image = mp.Image(
-          image_format=mp.ImageFormat.SRGB,
-          data=rgb_frame)
-
-        
-        timestamp_ms = int(time.time() * 1000) # Timestamp required for live mode
-
-        recognizer.recognize_async(mp_image, timestamp_ms)
-
-        
-        cv2.imshow("Gesture Recognition", frame) # Show camera feed
-
-        if cv2.waitKey(1) & 0xFF == 27:  # exit using ESC key
-          break
-
-    cap.release()
-    cv2.destroyAllWindows()
+  else:
+    quit
